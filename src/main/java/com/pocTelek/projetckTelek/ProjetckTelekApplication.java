6f7a15b5eed4f5fbb2b2a4ce4df56e9e9b323fe0
Robson Vieira <robson.ppmg@gmail.com>
2022-03-10 08:36:26 -0300

    subindo projeto

---
+++ src/main/java/com/pocTelek/projetckTelek/ProjetckTelekApplication.java
@@ -0,0 +1,13 @@
+package com.pocTelek.projetckTelek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetckTelekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetckTelekApplication.class, args);
+	}
+
+}
